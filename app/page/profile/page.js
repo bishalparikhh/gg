d28a6e60a55e25e0b25645ec769d70096d351e9a
DANTE-KUN3 <parikhbishal@gmail.com>
2025-07-29 13:37:18 +0530

    latest

--- app/page/profile/page.js
+++ app/page/profile/page.js
@@ -1,8 +1,12 @@
 "use client";
 
+import { useUser } from "@auth0/nextjs-auth0"
+
 import Sidebar from "../../components/sidebar/sidebar"
 
 export default function UserProfile(){ 
+
+const { user, isLoading } = useUser();  // Fetch user data directly
 
   if (isLoading) {
     return <div>Loading...</div>;  // You can show a loading state while the user data is loading
